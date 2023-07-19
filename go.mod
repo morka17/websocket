fd5a6121d9e1533a5f7888a71e90268ee36157b2
morka17 <joshuamorka19@gmail.com>
2023-07-19 01:09:52 +0100

    Broadcast: Implement casting timer

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/morka17/websocket
+
+go 1.20
+
+require golang.org/x/net v0.12.0
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+
+		msg := buf[:n]
+		s.broadCast(msg)
+	}
+}
+
+
+
+// Subscription
+func (s *Server) handleWSOrderbook(ws *websocket.Conn){
+	fmt.Println("New incomming connection from client to orderbook feed:", ws.RemoteAddr())
+
+	for {
+		payload := fmt.Sprintf("Orderbook data -> %d\n", time.Now().UnixNano())
+		ws.Write([]byte(payload))
+		time.Sleep(time.Second * 2)
+	}
+}
+
+// Websocket: Broadcast
+func (s *Server) broadCast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("Write error: ", err)
+			}
+		}(ws)
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/books", websocket.Handler(server.handleWSOrderbook))
+	http.ListenAndServe(":3000", nil)
+}
